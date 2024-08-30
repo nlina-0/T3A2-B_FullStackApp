2c4e8c322f54fd576f189eb183bbd6aec57cd290
Preedie <kiadslawson@gmail.com>
2024-08-30 18:02:39 +1000

    Test on Non Auth routes Complete classRoutes Revert

--- src/GymFlow-api/test/simple.test.js
+++
@@ -1,7 +0,0 @@
-import { User } from '../models/userModel.js';
-
-describe('Sample Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
---
+++ src/GymFlow-api/test/test.test.js
@@ -0,0 +1,7 @@
+
+
+describe('test of a test, testception', () => {
+  it('jest is pure evil pass if correct', () => {
+    expect(true).toBe(true);
+  });
+});
